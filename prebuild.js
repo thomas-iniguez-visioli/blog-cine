ce08ec3dcec8ca37a6cb409352956f11c511f05e
Thomas INIGUEZ-VISIOLI <tiniguezvi@290070u.local>
2025-01-07 09:33:12 +0100

    Auto-commit : changements locaux

--- prebuild.js
+++ prebuild.js
@@ -73,7 +73,7 @@ const geturl=async (name)=>{
 	return test.result[0].url
 }
 console.log(readdirSync("./source"))
-readdirSync("./source/_posts").map((filename)=>{
+readdirSync(path).map((filename)=>{
 	console.log(filename)
 	if(!forbiden.includes(filename)){
 		runAction(filename.split(".")[0]);
