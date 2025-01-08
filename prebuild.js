45e37db2e91d97b438c856279561c9249f67eb24
Thomas INIGUEZ-VISIOLI <tiniguezvi@290070u.local>
2025-01-08 11:48:20 +0100

    Auto-commit : changements locaux

--- prebuild.js
+++ prebuild.js
@@ -79,7 +79,7 @@ const runAction = (name) => {
 		geturl(name).then((result)=>{
 			console.log(result)
 		//	writeFileSync(`..\\cdn\\${name}.jpg`,result.url)
-			run(`curl "${result.replace('&amp;s','')}" -o ${name}.jpg `,__dirname+"/cdn")
+			run(`curl "${result.replace('&amp;s','')}" -o ${name}.jpg && `,__dirname+"/cdn")
 		}).catch((err)=>{log(err)})
 		
 		run(`curl https://thomas-iniguez-visioli.github.io/cdn/${name}.jpg -o dist/${name}.jpg `,__dirname)
