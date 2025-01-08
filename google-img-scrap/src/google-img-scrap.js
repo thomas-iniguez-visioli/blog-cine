8b5ea15c3c154e3671801b623b74a93f4e73704e
Thomas INIGUEZ-VISIOLI <tiniguezvi@290070u.local>
2025-01-08 09:50:02 +0100

    Auto-commit : changements locaux

--- google-img-scrap/src/google-img-scrap.js
+++ google-img-scrap/src/google-img-scrap.js
@@ -54,7 +54,7 @@ async function parse(url) {
 
   for (const script of scripts) {
     const body = script.innerHTML;
-    console.log
+    console.log(body)
     const valide = containImage(body);
 
     if (valide) {
