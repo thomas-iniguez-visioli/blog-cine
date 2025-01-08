f2847ff789562a7247e2ecd5ff9e0c535d2557f0
Thomas INIGUEZ-VISIOLI <tiniguezvi@290070u.local>
2025-01-08 09:34:54 +0100

    Auto-commit : changements locaux

--- prebuild.js
+++ prebuild.js
@@ -50,10 +50,9 @@ const run = (cmd, cwd) => execSync(cmd, { encoding: "utf8", stdio: "inherit", cw
 const parse=(array,file)=>{
 	const choix=array.pop()
 	if(choix.length!=0){
-		
+
 	}
-	run("npx hexo new post "+'"'+choix+'"',__dirname)
-	writeFileSync(file,array.join('\n'))
+	
 	parse(array)
 	
 }
