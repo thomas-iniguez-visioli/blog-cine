769890a23f0ae5529b464746e055908177ced944
Thomas INIGUEZ-VISIOLI <tiniguezvi@290070u.local>
2025-01-08 10:09:46 +0100

    Auto-commit : changements locaux

--- google-img-scrap/src/google-img-scrap.js
+++ google-img-scrap/src/google-img-scrap.js
@@ -60,7 +60,7 @@ async function parse(url) {
           if (n.name == "src") {
             console.log(n);
             
-            result.push({ url: ""+n.value.toString() });
+            result.push({ url: n.value.toString() });
           }
           if (n.name == "href") {
             console.log(n);
