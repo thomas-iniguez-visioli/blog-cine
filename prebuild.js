8f98fef14450f6f432d2b1cfab14c91f1c2cb801
Thomas INIGUEZ-VISIOLI <tiniguezvi@290070u.local>
2025-01-08 11:07:57 +0100

    Auto-commit : changements locaux

--- prebuild.js
+++ prebuild.js
@@ -77,7 +77,7 @@ const runAction = (name) => {
 		
 	}else{
 		geturl(name).then((result)=>{
-			writeFileSync(`..\\cdn\\${name}`,result)
+			writeFileSync(`..\\cdn\\${name}.jpg`,result)
 			run(` git add * */* && git commit -m "addimg"&& git pull && git push`,__dirname+"../cdn")
 		}).catch((err)=>{log(err)})
 		
